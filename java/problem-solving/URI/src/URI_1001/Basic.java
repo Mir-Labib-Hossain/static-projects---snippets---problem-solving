7607f0c6ae5711e147da5e3753fdc3ee0943ac6c
Mir Labib Hossain <68608228+MirLabib@users.noreply.github.com>
2022-01-13 19:32:58 +0600

    Add files via upload

---
+++ java/problem-solving/URI/src/URI_1001/Basic.java
@@ -0,0 +1,10 @@
+package URI_1001;
+import java.util.Scanner;
+public class Basic {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int A = obj.nextInt();
+        int B = obj.nextInt();
+        System.out.println("X = "+(A+B));
+    } 
+}
